a45baa17c1a879b08f4836e4a957f45a83d9654f
madbrain67 <ferrerofranck@yahoo.fr>
2021-09-28 16:51:33 +0200

    init

--- configuration/configuration.php
+++ configuration/configuration.php
@@ -10,3 +10,5 @@ date_default_timezone_set('Europe/Paris');
 $template = 'default';
 $language = isset($_GET['lang']) && !empty($_GET['lang']) ? $_GET['lang'] : 'fr';
 $languages = ['en', 'fr'];
+
+require_once ROOT.'/lang/'.$language.'.php';
--- view/default/index.php
+++ view/default/index.php
@@ -1,7 +1,3 @@
-<?php
-    require_once ROOT.'/lang/'.$language.'.php';
-?>
-
 <!doctype html>
 <html lang="<?= $language; ?>">
     <head>
